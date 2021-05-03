b057629cb5bda5ae912f783473e1ef59afa92229
khanhnguyen1999 <Macbookpro@MacBooks-MacBook-Pro-5.local>
2021-05-03 22:09:46 +0700

    done

---
+++ src/App.tsx
@@ -0,0 +1,94 @@
+import Badge from '@material-ui/core/Badge'
+import Drawer from '@material-ui/core/Drawer'
+import Grid from '@material-ui/core/Grid'
+import LinearProgress from '@material-ui/core/LinearProgress'
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart'
+import { useState } from 'react'
+import { useQuery } from 'react-query'
+// Styled
+import { StyledButton, Wrapper } from './Add.styles'
+import Cart from './Cart/Cart'
+// Components 
+import Item from './Item/item'
+
+
+// Types 
+export type CartItemType = {
+  id: number,
+  category: string,
+  description: string,
+  image: string,
+  price: number,
+  title: string,
+  amount: number
+}
+
+const getProducts = async (): Promise<CartItemType[]> =>
+  await (await fetch('https://fakestoreapi.com/products')).json()
+
+const App = () => {
+  const [cartOpen, setCartOpen] = useState(false)
+  const [cartItems, setCartItems] = useState([] as CartItemType[])
+
+  const { data, isLoading, error } = useQuery<CartItemType[]>('products', getProducts)
+
+  const getTotalItems = (items: CartItemType[]) =>
+    items.reduce((ack: number, item) => ack + item.amount, 0)
+  const handleAddToCart = (clickedItem: CartItemType) => {
+    setCartItems(prev => {
+      const isItemInCart = prev.find(item => item.id === clickedItem.id)
+
+      if (isItemInCart) {
+        return prev.map(item => (
+          item.id === clickedItem.id
+            ? { ...item, amount: item.amount + 1 } : item
+        ))
+      }
+      return [...prev, { ...clickedItem, amount: 1 }]
+    })
+  }
+  const handleRemoveFromCart = (id: number) => {
+    setCartItems(prev => (
+      prev.reduce((ack, item) => {
+        if (item.id === id) {
+          if (item.amount === 1) return ack;
+          return [...ack, {
+            ...item, amount: item.amount - 1
+          }]
+        } else {
+          return [...ack, item]
+        }
+      }, [] as CartItemType[])
+    ))
+  }
+
+  if (isLoading) return <LinearProgress />
+  if (error) return <div>Something went wrong...</div>
+  return (
+    <Wrapper>
+      <Drawer anchor="right" open={cartOpen} onClose={() => setCartOpen(false)}>
+        <Cart
+          cartItems={cartItems}
+          addToCart={handleAddToCart}
+          removeFromCart={handleRemoveFromCart}
+        />
+      </Drawer>
+      <StyledButton onClick={() => setCartOpen(true)}>
+        <Badge badgeContent={getTotalItems(cartItems)} color="error">
+          <AddShoppingCartIcon />
+        </Badge>
+      </StyledButton>
+      <Grid container spacing={3}>
+        {
+          data?.map((item, index) => (
+            <Grid item key={index} xs={12} sm={4}>
+              <Item item={item} handleAddToCart={handleAddToCart} />
+            </Grid>
+          ))
+        }
+      </Grid>
+    </Wrapper>
+  );
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import ReactDOM from 'react-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import App from './App';
+
+const client = new QueryClient()
+
+ReactDOM.render(
+  <QueryClientProvider client={client}>
+    <App />
+  </QueryClientProvider>,
+  document.getElementById('root')
+);
